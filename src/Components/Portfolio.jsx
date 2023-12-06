2de57889dfcc67288ae7c6fae88dd5adc22374ab
Sir Ronney Otieno <118272387+0tieno@users.noreply.github.com>
2023-12-06 07:42:13 +0000

    initial commit

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -37,19 +37,19 @@ const projectList = [
     url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Web Development for Beginners - A Curriculum",
     description:
       "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+    url: "0tieno.github.io/Food-Website/",
   },
   {
     title: "My Resume Site",
     description:
       "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+    url: "0tieno.github.io/Food-Website/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Food Website",
     description:
       "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
     url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
